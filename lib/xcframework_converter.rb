63a8520321a665661b0c0384cd60b03bcd94b921
Igor Makarov <igormaka@gmail.com>
2021-08-01 13:05:45 +0300

    add patching of `.swiftinterface` files as well

--- lib/xcframework_converter.rb
+++ lib/xcframework_converter.rb
@@ -76,6 +76,10 @@ module XCFrameworkConverter
       when :static
         patch_arm_binary_static(slice)
       end
+
+      slice.path.glob('**/arm64*.swiftinterface').each do |interface_file|
+        `sed -i '' -E 's/target arm64-apple-ios([0-9.]+) /target arm64-apple-ios\\1-simulator /g' "#{interface_file}"`
+      end
     end
 
     def patch_arm_binary_dynamic(slice)
