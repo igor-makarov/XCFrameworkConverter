cbdd37648a7f3e12e168fc853bf5dc52fffc68f0
Alex Zchut <a.zchut@applicaster.com>
2022-05-23 19:18:47 +0300

    fix to platform name compare

--- lib/xcframework_converter/patching.rb
+++ lib/xcframework_converter/patching.rb
@@ -20,13 +20,13 @@ module XCFrameworkConverter
       xcframework = Pod::Xcode::XCFramework.open_xcframework(xcframework_path)
 
       return nil if xcframework.slices.any? do |slice|
-        slice.platform == current_platform &&
+        slice.platform.name == current_platform.name &&
         slice.platform_variant == :simulator &&
         slice.supported_archs.include?('arm64')
       end
 
       original_arm_slice_identifier = xcframework.slices.find do |slice|
-        slice.platform == current_platform && slice.supported_archs.include?('arm64')
+        slice.platform.name == current_platform.name && slice.supported_archs.include?('arm64')
       end.identifier
 
       patched_arm_slice_identifier = "#{current_platform.name}-arm64-simulator"
